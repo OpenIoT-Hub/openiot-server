cf053fe2b436191f484e34f5e65a83bb2247b75b
Anxiu0101 <anxiu.fyc@foxmail.com>
2023-03-31 14:48:24 +0800

    [feat] err file in pkg

---
+++ api-gateway/biz/handler/err.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"github.com/anxiu0101/openiot-server/pkg/errno"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+type ErrResponse struct {
+	Code int64  `json:"status_code"`
+	Msg  string `json:"status_msg"`
+}
+
+func SendErrorResponse(c *app.RequestContext, err error) {
+	errno := errno.ConvertErr(err)
+	c.JSON(consts.StatusOK, ErrResponse{
+		Code: errno.ErrorCode,
+		Msg:  errno.ErrorMsg,
+	})
+}
+
+func SendCommonResponse(c *app.RequestContext, data interface{}) {
+	c.JSON(consts.StatusOK, data)
+}
+
+// PhaseToken returns Userid
+func PhaseToken(token string) (int64, error) {
+	//return rpc.CheckToken(context.Background(), &user.CheckTokenRequest{
+	//	Token: token,
+	//})
+	return 0, nil
+}
---
+++ api-gateway/biz/handler/user.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+// UserRegister .
+// @router /openiot/user/register [POST]
+func UserRegister(ctx context.Context, c *app.RequestContext) {
+	//var err error
+	//var req user.UserRegisterRequest
+	//err = c.BindAndValidate(&req)
+	//if err != nil {
+	//	SendErrorResponse(c, errno.ParamError.WithMessage(err.Error()))
+	//	return
+	//}
+	//
+	//userId, token, err := rpc.UserRegister(context.Background(), &user.UserRegisterRequest{
+	//	Username: req.Username,
+	//	Password: req.Password,
+	//})
+	//
+	//if err != nil {
+	//	SendErrorResponse(c, err)
+	//	return
+	//}
+	//
+	//SendCommonResponse(c, &basic.UserRegisterResponse{
+	//	StatusCode: errno.SuccessCode,
+	//	StatusMsg:  errno.SuccessMsg,
+	//	Token:      token,
+	//	UserId:     userId,
+	//})
+}
+
+// UserLogin .
+// @router /openiot/user/login [POST]
+func UserLogin(ctx context.Context, c *app.RequestContext) {
+	//var err error
+	//var req basic.UserLoginRequest
+	//err = c.BindAndValidate(&req)
+	//if err != nil {
+	//	SendErrorResponse(c, errno.ParamError.WithMessage(err.Error()))
+	//	return
+	//}
+	//
+	//userId, token, err := rpc.UserLogin(context.Background(), &user.UserLoginRequest{
+	//	Username: req.Username,
+	//	Password: req.Password,
+	//})
+	//
+	//if err != nil {
+	//	SendErrorResponse(c, err)
+	//	return
+	//}
+	//
+	//SendCommonResponse(c, &basic.UserLoginResponse{
+	//	StatusCode: errno.SuccessCode,
+	//	StatusMag:  errno.SuccessMsg,
+	//	UserId:     userId,
+	//	Token:      token,
+	//})
+}
