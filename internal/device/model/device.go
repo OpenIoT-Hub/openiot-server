e1e3650c1235c3ef4dd07d54fbf34c21baced31e
Anxiu0101 <anxiu.fyc@foxmail.com>
2023-04-21 16:00:38 +0800

    [feat] update device idl
    Signed-off-by: Anxiu0101 <anxiu.fyc@foxmail.com>

--- internal/device/model/device.go
+++ internal/device/model/device.go
@@ -3,13 +3,15 @@ package model
 import "gorm.io/gorm"
 
 type (
-	Device struct {
+	// BasicDevice 设备，核心类，external 存放一个 JSON，储存可变参数
+	BasicDevice struct {
 		gorm.Model
-		longitude float64
-		latitude  float64
-		version   int64  // 可变参数版本
-		state     int64  // 设备状态码
-		external  string // 可变参数
+		Longitude float64 `gorm:"column:longitude"`
+		Latitude  float64 `gorm:"column:latitude"`
+		Version   int64   `gorm:"column:version"`  // 可变参数版本
+		State     int64   `gorm:"column:state"`    // 设备状态码
+		Comment   string  `gorm:"column:comment"`  // 备注
+		External  string  `gorm:"column:external"` // 可变参数
 	}
 
 	// DeviceV1 The temp Device struct
